f5d533000e1a5c552f4cf1cb9832e01a313ab8cf
Stephan Chilingaryan <stephan@komikku.us>
2011-11-07 10:42:18 -0800

    Added main menu

--- ClumsyChef/ClumsyChef/CHMainMenuLayer.m
+++ ClumsyChef/ClumsyChef/CHMainMenuLayer.m
@@ -8,16 +8,121 @@
 
 #import "CHMainMenuLayer.h"
 
+// private methods are declared in this manner to avoid "may not respond to ..." compiler warnings
+@interface CHMainMenuLayer (PrivateMethods)
+-(void) createMenu:(ccTime)delta;
+-(void) goBackToPreviousScene;
+-(void) changeScene:(id)sender;
+-(void) menuItem1Touched:(id)sender;
+-(void) menuItem2Touched:(id)sender;
+-(void) menuItem3Touched:(id)sender;
+@end
 
 @implementation CHMainMenuLayer
 
+CCSprite *background;
 
-- (id)init
++(id) scene
 {
-	if (self = [super init])
+	CCScene* scene = [CCScene node];
+	CCLayer* layer = [CHMainMenuLayer node];
+	[scene addChild:layer];
+	return scene;
+}
+
+-(id) init
+{
+	if ((self = [super init]))
 	{
+		CCLOG(@"init %@", self);
 		
+		// wait a short moment before creating the menu so we can see it scroll in 
+        background = [CCSprite spriteWithFile:@"MainMenuBackground.png"];
+        background.position = CGPointMake(background.contentSize.width/2,background.contentSize.height/2);
+        [self addChild:background];
+        
+        // set CCMenuItemFont default properties
+        [CCMenuItemFont setFontName:@"Helvetica-BoldOblique"];
+        [CCMenuItemFont setFontSize:20];
+        
+        // create a few labels with text and selector
+        CCMenuItemFont* item1 = [CCMenuItemFont itemFromString:@"High Scores!" target:self selector:@selector(menuItem1Touched:)];
+        
+        // create a menu item using existing sprites
+        CCSprite* normal = [CCSprite spriteWithFile:@"play.png"];
+        CCSprite* selected = [CCSprite spriteWithFile:@"play2.png"];
+        CCMenuItemSprite* item2 = [CCMenuItemSprite itemFromNormalSprite:normal selectedSprite:selected target:self selector:@selector(menuItem2Touched:)];
+        
+        // create a toggle item using two other menu items (toggle works with images, too) 	 [CCMenuItemFont setFontName:@"STHeitiJ-Light"];
+        
+        CCMenuItemFont* toggleOn = [CCMenuItemFont itemFromString:@"I'm ON!"];
+        CCMenuItemFont* toggleOff = [CCMenuItemFont itemFromString:@"I'm OFF!"];
+        CCMenuItemToggle* item3 = [CCMenuItemToggle itemWithTarget:self selector:@selector(menuItem3Touched:) items:toggleOn, toggleOff, nil];
+        
+        CCMenu* menu = [CCMenu menuWithItems:item1, item2, item3, nil];
+        menu.anchorPoint = CGPointZero;
+        menu.position = CGPointZero;
+        
+        menu.tag = 100;
+        
+        // create the menu using the items
+        item1.position = ccp(20, 160);
+        item2.position = ccp(170, 220);
+        item3.position = ccp(item2.position.x, 100);
+        
+        [self addChild:menu];
 	}
 	return self;
 }
+
+-(void) goBackToPreviousScene
+{
+	// get the menu back
+	CCNode* node = [self getChildByTag:100];
+	NSAssert([node isKindOfClass:[CCMenu class]], @"node is not a CCMenu!");
+    
+	CCMenu* menu = (CCMenu*)node;
+    
+	// lets move the menu out using a sequence
+	CGSize size = [[CCDirector sharedDirector] winSize];
+	CCMoveTo* move = [CCMoveTo actionWithDuration:1 position:CGPointMake(-(size.width / 2), size.height / 2)];
+	CCEaseBackInOut* ease = [CCEaseBackInOut actionWithAction:move];
+	CCCallFunc* func = [CCCallFunc actionWithTarget:self selector:@selector(changeScene:)];
+	CCSequence* sequence = [CCSequence actions:ease, func, nil];
+	[menu runAction:sequence];
+}
+
+-(void) changeScene:(id)send
+{
+	//[[CCDirector sharedDirector] replaceScene:[HelloWorld scene]];
+}
+
+-(void) menuItem1Touched:(id)sender
+{
+	CCLOG(@"item 1 touched: %@", sender);
+	[self goBackToPreviousScene];
+}
+
+-(void) menuItem2Touched:(id)sender
+{
+	CCLOG(@"item 2 touched: %@", sender);
+}
+
+-(void) menuItem3Touched:(id)sender
+{
+	// sender is a CCMenuItemToggle in this case
+	CCMenuItemToggle* toggleItem = (CCMenuItemToggle*)sender;
+	int index = [toggleItem selectedIndex];
+	
+	CCLOG(@"item 3 touched: %@ - selected index: %i", sender, index);
+}
+
+-(void) dealloc
+{
+	CCLOG(@"dealloc: %@", self);
+	
+	// always call [super dealloc] at the end of every dealloc method
+	[super dealloc];
+}
+
 @end
