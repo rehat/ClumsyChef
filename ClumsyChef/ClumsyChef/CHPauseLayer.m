26beb20f2025bf3848106cb1280c1c7b7430e1c9
Donald Stern <donald@Donald-Sterns-MacBook-Pro.local>
2011-11-21 03:15:29 -0800

    Working on pause layer

--- ClumsyChef/ClumsyChef/CHPauseLayer.m
+++ ClumsyChef/ClumsyChef/CHPauseLayer.m
@@ -7,7 +7,38 @@
 //
 
 #import "CHPauseLayer.h"
+#import "CHGameScene.h"
 
 @implementation CHPauseLayer
+
+- (id)init
+{
+	if (self = [super init])
+	{
+		//CCSprite *chefLife = [CCSprite spriteWithFile:@"hud-chefLive.png"];
+		//[chefLife setPositionSharp:ccp(250, 30)];
+		//[self addChild:chefLife];
+        
+        
+        CCMenuItemImage *item = [CCMenuItemImage itemFromNormalImage:@"playbutton.png" 
+                                                       selectedImage:@"playbuttonselected.png" 
+                                                              target:self 
+                                                            selector:@selector(resumeButtonPressed)];
+        CCMenu *menu = [CCMenu menuWithItems:item, nil];
+        [menu setPositionSharp:ccp(250, 30)];
+        [self addChild:menu];
+        
+        
+		        
+    }
+	return self;
+}
+
+- (void)resumeButtonPressed
+{
+	// TODO
+}
+
+
 
 @end
